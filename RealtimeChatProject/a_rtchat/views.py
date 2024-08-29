2e04fc2fb4a27d7534130d4271bf0fcdb9c1f22c
usman.shahid <usmanshahid027@outlook.com>
2024-08-28 17:55:01 -0700

    added model for chat groups and messages

--- RealtimeChatProject/a_rtchat/views.py
+++ RealtimeChatProject/a_rtchat/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def chat_view(request):
+    chat_group = get_object_or_404(
+        ChatGroup,
+        group_name="Public Chat"
+    )
+    chat_messages = chat_group.chat_messages.all()[:30]
     return render(
         request,
         'a_rtchat/chat.html',
